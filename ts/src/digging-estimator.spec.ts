2db7dee70cb324c6ecafd8a939aaf3c76cc07d47
ludovicGendre <ludovic.gendre@g-mail.fr>
2022-10-21 16:52:11 +0200

    feat: add test fake night and day team for tunnel 28l and 2d

--- ts/src/digging-estimator.spec.ts
+++ ts/src/digging-estimator.spec.ts
@@ -1,4 +1,4 @@
-import { DiggingEstimator } from "./digging-estimator";
+import { DiggingEstimator, Team } from "./digging-estimator";
 
 const myMock = jest.fn();
 class MockDiggingEstimator extends DiggingEstimator {
@@ -29,12 +29,45 @@ describe("digging estimator", () => {
     expect(result.total).toBe(48);
   });
 
+  it("should fake dayTeam for tunnel 28 length and 2 days", () => {
+    const test: Team = {
+      miners: 3,
+      healers: 1,
+      smithies: 2,
+      lighters: 0,
+      innKeepers: 8,
+      guards: 0,
+      guardManagers: 0,
+      washers: 2,
+    };
+
+    const estimator = new MockDiggingEstimator([0, 3, 5.5, 7]);
+    const result = estimator.tunnel(28, 2, "granite");
+    expect(result.dayTeam).toEqual(test);
+  });
+
+  it("should fake nightTeam for tunnel 28 length and 2 days", () => {
+    const test: Team = {
+      miners: 3,
+      healers: 1,
+      smithies: 2,
+      lighters: 4,
+      innKeepers: 12,
+      guards: 5,
+      guardManagers: 2,
+      washers: 3,
+    };
+
+    const estimator = new MockDiggingEstimator([0, 3, 5.5, 7]);
+    const result = estimator.tunnel(28, 2, "granite");
+    expect(result.nightTeam).toEqual(test);
+  });
 });
 
 describe("Throw Errors", () => {
   it("should throw an error when too long delay exception", () => {
     const estimator = new MockDiggingEstimator([0, 3, 5.5, 7]);
-  
+
     expect(() => {
       estimator.tunnel(45, 2, "granite");
     }).toThrowError();
