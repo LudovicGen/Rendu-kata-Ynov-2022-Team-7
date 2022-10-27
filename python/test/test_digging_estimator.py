611ea66fe642c3d6038227282d847937ab951396
ludovicGendre <ludovic.gendre@g-mail.fr>
2022-10-27 15:00:07 +0200

    chore: update archi for just ts and add husky precheck commit launch test

--- python/test/test_digging_estimator.py
+++
@@ -1,9 +0,0 @@
-from src.digging_estimator import DiggingEstimator
-
-
-def test_returns_as_doctor_Pockosky_says():
-    estimator = DiggingEstimator()
-
-    result = estimator.tunnel(28, 2, "granite")
-
-    assert result.total == 48
